160c587a8adbde020d61d4e1a4c776e3786efd8a
serkankeklik <36251060+serkankeklik@users.noreply.github.com>
2026-05-22 00:27:14 +0100

    Upload setup.py from Pyto

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+from make_sphinx_documentation import BuildDocsCommand
+
+BuildDocsCommand.sourcedir = "docs"
+
+setup(
+    cmdclass={
+        'build_docs': BuildDocsCommand, # Run to build the documentation
+    },
+
+    ext_modules = [
+        Extension("dosojin.dosojin", ["dosojin/dosojin.c"])
+    ],
+    
+    include_package_data=True,
+)
